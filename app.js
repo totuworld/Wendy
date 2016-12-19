19c6d29468642f4a0cda495a8b1b55bb33b99482
totuworld <totuworld@gmail.com>
2016-12-19 16:51:38 +0900

    update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.send({
             message: err.message,
             error: err
         });
@@ -48,7 +48,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.send({
         message: err.message,
         error: {}
     });
